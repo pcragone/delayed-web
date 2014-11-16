fa61aa76d5cd98a3e042dea8daed897d9776a8b0
pccr <pcragone@gmail.com>
2014-11-16 17:18:31 -0500

    Add async reload via jQuery.

---
+++ app/assets/javascripts/delayed/web/application.js
@@ -0,0 +1,30 @@
+//= require jquery-1.11.1.min
+
+function addZero(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+function getTime() {
+  var d = new Date();
+  var h = addZero(d.getHours());
+  var m = addZero(d.getMinutes());
+  var s = addZero(d.getSeconds());
+  return h + ":" + m + ":" + s;
+}
+
+loadJobs = function () {
+  var time = new Date();
+
+  jQuery('table #jobRows').load('jobs.html #jobRows tr', function() {
+    $('#lastUpdated').html(getTime());
+  });
+};
+
+jQuery(document).ready(function() {
+  setInterval(loadJobs, 3000);
+
+  $('#loadNow').click(loadJobs);
+});
